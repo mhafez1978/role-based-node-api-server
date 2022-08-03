97d345c4ce7bfc938eab06786a6500483f567182
Mohamed Hafez <mhafez1978@outlook.com>
2022-08-03 01:25:07 -0400

    added connect flash for flash messages , added express session

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const StartMyApp = () => {
   const createHttpError = require('http-errors');
   const morgan = require('morgan');
   const db = require('mongoose');
+  const session = require('express-session');
+  const flash = require('connect-flash');
+
   const app = express();
+
+  
 
   // lets set the app to use specific views engine in this case ejs, but could use anything else ...
   app.set('view engine', 'ejs');
@@ -13,12 +18,36 @@ const StartMyApp = () => {
   // lets register public folder with express so we can use in our project
   app.use('/public', express.static(path.resolve('./public')));
   // app.use(express.static('public'))
-
-
-  
-
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+  
+  // init session this will be used in connect flash package and in passport for auth ...
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    // stop from saving in session store
+    resave: false,
+    // stop from saving in session store, help with race conditions ...
+    saveUninitialized:false,
+    // let's initialize the browser session cookie
+    cookie:{
+      // when using https://
+      // secure:true
+      // since we in dev now will comment it out
+      
+      // the following will prevent cookie snooping software or browsers from reading our cookie
+      // only server can read ...
+      httpOnly:true,
+    }
+  }));
+
+  // init connect flash messages
+  app.use(flash());
+  app.use((req,res,next)=>{
+    res.locals.messages = req.flash();
+    next();
+  })
 
   // App Routes
   app.use('/', require('./router/index.route'));
@@ -27,8 +56,7 @@ const StartMyApp = () => {
   app.use('/auth', require('./router/auth.route'));
 
   // let's log requests in console
-  app.use(morgan('dev'));
-
+  
 
   // handling errors - part1
   // if route is unknown to our app our error handling should be triggered
@@ -55,7 +83,7 @@ const StartMyApp = () => {
       // after we've connecteed to the db server then we start the web server ...
       app.listen(config.PORT, () => {
         console.log(
-          `Your application server is now running on http://localhost:${config.PORT}`
+          `Your application server is now running on http://localhost:${config.PORT}\n\n`
         );
       });
     })
--- router/auth.route.js
+++ router/auth.route.js
@@ -4,13 +4,24 @@ const controllers = require('../controllers/index')
 router.get('/login', (req, res, next) => {
   res.render('login');
 });
-
-router.post('/login', (req, res, next) => {});
-
-router.get('/register', (req, res, next) => {
-  res.render('register');
+router.post('/login', (req, res, next) => {
+  res.send('logging you in now ...');
 });
 
+router.get('/register', async(req, res, next) => {
+  // req.flash('alert-danger','Waring all fields are required before we complete your request ...');
+  // req.flash('alert-info','Hmmm, looks like more information is needed ...');
+  // req.flash('alert-success','Great job, you\'ve signed up successfully ...');
+  // req.flash('alert-primary','Great job, you\'ve signed up successfully ...');
+  // req.flash('alert-secondary','Great job, you\'ve signed up successfully ...');
+  // req.flash('alert-warning','Great job, you\'ve signed up successfully ...');
+  // req.flash('alert-light','Great job, you\'ve signed up successfully ...');
+  // req.flash('alert-dark','Great job, you\'ve signed up successfully ...');
+  // // const messages = req.flash()
+  // // lets create a onetime use only flash message to use 
+  // res.redirect('/auth/login');
+  res.render('register')
+});
 router.post('/register', (req, res, next) => {
   return controllers.register(req,res,next)
 });
